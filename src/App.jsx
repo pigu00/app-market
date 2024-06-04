6af4b15d8d7738afde07546868e9589cb0c4971d
robertomarban <robertomarban@gmail.com>
2024-06-04 10:31:26 +0000

    initial push y hero home

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Hero from './components/home/hero'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
